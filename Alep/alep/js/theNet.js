1b681fa72c67bb617bc55c8b121bc3cdd47999de
c0de-xs <animo.phoenicus@gmail.com>
2021-11-25 18:42:44 +0100

    tidy up

--- Alep/alep/js/theNet.js
+++ Alep/alep/js/theNet.js
@@ -1,18 +1,9 @@
 const pi = document.querySelector(".the-net");
-console.log('ok');
-  pi.addEventListener("click", function ()
-  {
-  console.log('pressed');
-});
-pi.addEventListener('keydown', function (event) { 
-  event.preventDefault();
-if (event.ctrlKey && event.shiftKey) { 
-  console.log('Ctrl+shift pressed')
-}
-});
+
 pi.addEventListener('click', function (e) { 
   if (e.ctrlKey && e.shiftKey) { 
-    console.log('ctrl+shift+click'); 
+    console.log('ctrl+shift+click');
+    window.location = "/secret/the-net.html"; 
     return; } else {
       window.alert('"You know nothing, Jon Snow!"')
     }});
