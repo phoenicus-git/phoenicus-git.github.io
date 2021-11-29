dbf285daee49db8696b1e49158a57d442922a304
c0de-xs <animo.phoenicus@gmail.com>
2021-11-29 16:31:44 +0100

    add google analytics for testing

--- Alep/alep/js/alep-cookies.js
+++ Alep/alep/js/alep-cookies.js
@@ -12,9 +12,15 @@ const cookieBanner = `<div class="cookie-banner">
       <button class="cookie-reject">Reject</button>
     </div><!--.cookie-buttons-->
   </div><!--cookie-banner-->`;
+const cookieScripts = document.querySelectorAll(`script[type="text/plain"]`);
+console.log(cookieScripts);
 const permissionLocal = localStorage.getItem(`cookies`);
 const permissionSession = sessionStorage.getItem(`cookies`);
 if (permissionLocal == `permissionGranted`) {
+  cookieScripts.forEach(function(accepted){
+    accepted.removeAttribute(`type`);
+  console.log(`accepted`);
+  });
   console.log(`granted`);
   return;
   
@@ -26,9 +32,13 @@ insertBanner.insertAdjacentHTML(`afterend`, cookieBanner);
 const banner = document.querySelector(`.cookie-banner`)
 const acceptCookiesBtn = document.querySelector(`.cookie-accept`);
 const rejectCookieBtn = document.querySelector(`.cookie-reject`);
+
 acceptCookiesBtn.addEventListener(`click`, function() {
   localStorage.setItem(`cookies`, `permissionGranted`);
+  cookieScripts.forEach(function(accepted){
+    accepted.removeAttribute(`type`);
   console.log(`accepted`);
+  });
   banner.remove();
   console.log(`removed`);
 });
