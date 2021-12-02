8287f0f42b449b17d11ae0c776b988c1858ea378
AnimoPhoenicus <animo.phoenicus@gmail.com>
2021-12-02 13:49:22 +0100

    eslint free

--- Alep/alep/js/warGames2.js
+++ Alep/alep/js/warGames2.js
@@ -1,5 +1,6 @@
 // warGames.js v.0.0.1 easter egg | MIT License | https://www.phoenicus.com/
 // The script will simulate the login from Wargames.
+(function (){
 `use strict`;
   const insRsp = document.querySelector(`.text`);
   const submit = document.querySelector(`.submit`);
@@ -8,6 +9,7 @@
   const errorRsp = `<div class="resp-text"><h2> IDENTIFICATION NOT RECOGNIZED BY SYSTEM</h2><p class="blink">--CONNECTION TERMINATED--</p><button class="resp-btn">Ok</button></div>`;
   const helpLogon = `<div class="resp-text"><h2> HELP NOT AVAILABLE</h2><button class="resp-btn">Ok</button></div>`;
   const helpGames = `<div class="resp-text"><h2>'GAMES' REFERS TO MODELS, SIMULATIONS AND GAMES WHICH HAVE TACTICAL AND STRATEGIC APPLICATIONS.</h2><button class="resp-btn">Ok</button></div>`;
+  const listGames = `<div class="resp-text"><h2 class="list">FALKEN'S MAZE<br>BLACK JACK<br>GIN RUMMY<br>HEARTS<br>BRIDGE<br>CHECKERS<br>CHESS<br>POKER<br>FIGHTER COMBAT<br>GUERILLA ENGAGEMENT<br>DESERT WARFARE<br>AIR-TO-GROUND ACTIONS<br>THEATERWIDE TACTICAL WAR<br>THEATERWIDE BIOTOXIC AND CHEMICAL WARFARE<br><br><span class="blink">GLOBAL THERMONUCLEAR WAR</span></h2><button class="resp-btn">Ok</button></div>`;
   const logon = `Joshua`;
   const helpLog = `Help Logon`;
   const helpG = `Help Games`;
@@ -22,7 +24,7 @@
           rspText.remove();
           document.querySelector(`.input`).value = ``;
       });
-  };
+  }
   const success = () => window.location = `/secret/the-net.html`; 
   submit.addEventListener(`click`, function () { 
     let userLogon = document.querySelector(`.input`).value;
@@ -44,3 +46,4 @@
         remRsp();
       }
   });
+})();
